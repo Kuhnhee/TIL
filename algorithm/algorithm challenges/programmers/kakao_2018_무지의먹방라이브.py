0fe10418066d821c36e5e4dd0756e7bf62f389bb
Kuhnhee <sheva0902@naver.com>
2019-09-09 08:41:40 +0900

    190909

--- algorithm/algorithm challenges/programmers/kakao_2018_무지의먹방라이브.py
+++ algorithm/algorithm challenges/programmers/kakao_2018_무지의먹방라이브.py
@@ -1,25 +1,41 @@
 def solution(food_times, k):
+    food_times_dic = {}
+    food_times_list = []
+    totalTime = 0
 
-    foods = [[food, idx+1] for idx, food in enumerate(food_times)]
+    for i in range(0, len(food_times)):
+        food_times_list.append([i, food_times[i]])
+        totalTime+=food_times[i]
 
-    idx = 0
-    while k>0:
-        #eat
-        foods[idx][0] -= 1
-        if foods[idx][0] == 0:
-            foods.remove(foods[idx])
-            if idx == len(foods):
-                idx = 0
-        else:
-            idx = (idx+1)%len(foods)
+    if totalTime <= k:
+        return -1
 
-        if not foods:
-            return -1
+    food_times_list = sorted(food_times_list, key=lambda x:x[1])
 
-        k-=1
+    delTime = food_times_list[0][1]*len(food_times_list)
+    i=1
+    # print k
+    # print delTime
+    while delTime < k:
+        k-=delTime
+        delTime = (food_times_list[i][1]-food_times_list[i-1][1])*(len(food_times_list)-i)
+        # print k, delTime
+        i+=1
 
-    answer = foods[idx][1]
-    return answer
+    print(food_times_list)
+    food_times_list = sorted(food_times_list[i-1:], key=lambda x:x[0])
+    # print food_times_list
+    # print k
+    print(i)
+    print(food_times_list)
+    print(k%len(food_times_list))
+    return food_times_list[k%len(food_times_list)][0]+1
 
 if __name__ == "__main__":
-    solution([3, 1, 2], 5)
+    # solution([3, 1, 2], 5)
+    # solution([5,4,1], 200)
+    # solution([7,3,4,2,5,6], 25)
+    # solution([3,1,1,1,2,4,3],12)
+    a = solution([4, 3, 5, 6, 2], 7)
+    # a = solution([1000,500,750], 10000)
+    print("answer was...", a)
